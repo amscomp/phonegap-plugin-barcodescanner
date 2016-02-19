f12e1745d3c24e3080266d3660aa3f06520d1f38
amscomp <amscomp@users.noreply.github.com>
2016-02-19 15:16:48 -0800

    Increased Viewfinder Size

--- src/android/LibraryProject/src/com/google/zxing/client/android/camera/CameraManager.java
+++ src/android/LibraryProject/src/com/google/zxing/client/android/camera/CameraManager.java
@@ -41,10 +41,10 @@ public final class CameraManager {
 
   private static final String TAG = CameraManager.class.getSimpleName();
 
-  private static final int MIN_FRAME_WIDTH = 220;
-  private static final int MIN_FRAME_HEIGHT = 220;
-  private static final int MAX_FRAME_WIDTH = 880;
-  private static final int MAX_FRAME_HEIGHT = 680;
+  private static final int MIN_FRAME_WIDTH = 660;
+  private static final int MIN_FRAME_HEIGHT = 660;
+  private static final int MAX_FRAME_WIDTH = 1475;
+  private static final int MAX_FRAME_HEIGHT = 800;
 
   private final Context context;
   private final CameraConfigurationManager configManager;
